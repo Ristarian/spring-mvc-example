d92e1c255e902b498f5bc9d750ffcfedb4b3179d
Sebastian <sebastian.gornicki@protonmail.com>
2019-07-07 13:39:16 +0200

    New commit (7/7/2019)

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -1,5 +1,50 @@
 package controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import javax.sql.rowset.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/cars")
 public class CarController {
+
+    List<Car> cars = new ArrayList<Car>();
+
+    CarController(){
+        cars.add(new Car(1, "BMV", 740));
+        cars.add(new Car(2, "Opel", 620));
+    }
+
+    @GetMapping
+    @ResponseBody
+    List<Car> getCars(){
+        return cars;
+    }
+
+    @GetMapping("/mark")
+    ResponseEntity<List<Car>> getCarByMark(@RequestParam(value = "mark", defaultValue = "BMV")String mark){
+        return ResponseEntity
+                .ok(getCarsBy(car -> mark.equals(car.getMark())));
+    }
+
+   private List<Car> getCarsBy(Predicate<Car> predicate) {
+       return cars.stream()
+               .filter(predicate)
+               .collect(Collectors.toList());
+   }
+
+   @GetMapping("/{mark:[A-Za-z]}}")
+    @ResponseBody
+    List<Car> getCarsByMarkParthVariable(@PathVariable("mark")String mark){
+        return getCarsBy(car -> mark.equals(car.getMark()));
+   }
+
+
+
 }
+
